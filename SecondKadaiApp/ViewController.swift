cc52f2b555362cc3f8d3c84bace28254e1a5e052
yasushi.gushiken <yassii_g@hotmail.com>
2019-05-09 21:28:24 +0900

    提出１

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var namae: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    //namae2 = namae.test
+        //let test = ""
+        //namae.text = "test"
+        //namae:UITextField = UITextField()
+        //let namae = "aaaa"
+        //let namae2 = "\(namae.text)"
+        //namae.text = "namae3"
+        //namae : UITextField = UITextField()
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているnamaeに値を代入して渡す
+        resultViewController.namae2 = "\(namae.text!)"
+        //resultViewController.namae = result
+        
+        
+        
     }
 
-
+    @IBAction func unwind(_ segue: UIStoryboardSegue){  //画面戻る
+        
+    }
 }
 
